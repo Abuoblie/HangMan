90791522718b5e3ab3f33d1495d76067f36a3870
Abuoblie <Abu.oblie@gmail.com>
2021-02-11 08:28:56 +0100

    finish project

--- Script.js
+++ Script.js
@@ -1,3 +1,16 @@
+const canvas = document.getElementById("canvasarea");
+const context = canvas.getContext('2d');
+let xPos = 20, yPos = canvas.height-20;
+let width = canvas.width-40, height=10;
+context.fillStyle='green';
+context.fillRect(xPos, yPos, width, height);
+context.fillRect(canvas.width/2 -60, yPos, 30, height-120);
+context.fillRect(canvas.width/2 -60, yPos-100, width-120, height);
+context.fillStyle='black'; 
+context.beginPath();
+context.moveTo(canvas.width/2 +30, yPos-90);
+context.lineTo(canvas.width/2 +30, yPos-80);
+context.stroke();
 
 document.getElementById("button").innerHTML = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".replace(/\S/g, "<button class='char'>$&</button>");
 const wordlist = ["cord","handle","order","mind","wacky","grade","flashy","geese","veil","bushes","fall","wish","desire","rainy","terrify","cease","rigid","creature","crackle"];
@@ -40,9 +53,13 @@ document.getElementById("reset").addEventListener("click",
             lives.innerHTML = MaxWrongchoices;
             
             document.getElementById("answer").innerHTML = Answer;
+            canvas.i
+            canvas.style.backgroundColor ="white";
+            context.clearRect(canvas.width/2 -30, yPos-80, 120, 80);
+            
         }
         
-        )
+ )
 
 
 document.getElementById("answer").innerHTML = Answer;
@@ -67,8 +84,50 @@ bArray.forEach($btn =>{
            
             if (Correct === 0) {
                 Wrong = Wrong +1;
-                
             }
+            if (Wrong === 1) {
+                context.beginPath();
+                context.arc(180, 60, 10, 0, Math.PI*2, true);
+                context.stroke(); 
+                context.closePath();     
+            }
+            if (Wrong === 2) {
+                context.beginPath();
+                context.moveTo(canvas.width / 2 + 30, yPos - 60);
+                context.lineTo(canvas.width / 2 + 30, yPos - 30);
+                context.stroke();
+                context.closePath();
+            }
+            if (Wrong === 3) {
+                context.beginPath();
+                context.moveTo(canvas.width / 2 + 30, yPos - 55);
+                context.lineTo(canvas.width / 2 +5, yPos-50 );
+                context.stroke();
+                context.closePath();
+            }
+            if (Wrong === 4) {
+                context.beginPath();
+                context.moveTo(canvas.width / 2 + 30, yPos - 55);
+                context.lineTo(canvas.width / 2 + 50, yPos-50 );
+                context.stroke();
+                context.closePath();
+            }
+            if (Wrong === 5) {
+                context.beginPath();
+                context.moveTo(canvas.width / 2 + 30, yPos - 30);
+                context.lineTo(canvas.width / 2 +5, yPos-20 );
+                context.stroke();
+                context.closePath();
+            }
+            if (Wrong === 6) {
+                context.beginPath();
+                context.moveTo(canvas.width / 2 + 30, yPos - 30);
+                context.lineTo(canvas.width / 2 +50, yPos-20 );
+                context.stroke();
+                context.closePath();
+                canvas.style.backgroundColor ="red";    
+            }
+
              lives.innerHTML = MaxWrongchoices-Wrong;
            
             if (CorrectL ===  hiddenWord.length) {
@@ -78,6 +137,8 @@ bArray.forEach($btn =>{
                 document.getElementById("result").innerHTML = "congrats you\'ve wone click reset to start another game" ;
                 document.getElementById("result").style.backgroundColor = "green";
                 document.getElementById("result").style.color = "white";
+                canvas.style.backgroundColor ="green";
+                context.clearRect(canvas.width/2 -30, yPos-90, 120, 80);
             }
             else if (Wrong === MaxWrongchoices) {
                 document.getElementById("result").style.backgroundColor = "red";
